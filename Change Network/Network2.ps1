698c259caa05369680f0179d47ba18252032dd97
Ishaan Malhotra <iamfire2005@yahoo.com>
2021-06-09 12:24:09 +0800

    Add files via upload

---
+++ Change Network/Network2.ps1
@@ -0,0 +1,20 @@
+param([switch]$Elevated)
+function Check-Admin {
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+if ((Check-Admin) -eq $false) {
+if ($elevated)
+{
+# could not elevate, quit
+}
+else {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+}
+exit
+}
+
+Get-NetAdapter -Name Network2 | Enable-NetAdapter
+Get-NetAdapter -Name Network1 | Disable-NetAdapter -Confirm:$false
+
+stop-process $pid
